3361d43e9c22c6ccc839e79f6c996110fd95fa6a
therahulkumar <rahul.cybo@gmail.com>
2019-06-10 11:40:56 +0530

    added docker

---
+++ docker/app.py
@@ -0,0 +1,47 @@
+import json
+from flask import Flask
+import keras
+from keras_retinanet import models
+from keras_retinanet.utils.image import read_image_bgr, preprocess_image, resize_image
+import numpy as np
+import time
+
+import tensorflow as tf
+
+#def get_session():
+#    config = tf.ConfigProto()
+#    config.gpu_options.allow_growth = True
+#    return tf.Session(config=config)
+#
+#keras.backend.tensorflow_backend.set_session(get_session())
+
+app = Flask(__name__)
+
+
+model = models.load_model('resnet50_coco_best_v2.1.0.h5', backbone_name='resnet50')
+graph = tf.get_default_graph()
+
+
+labels_to_names = {0: 'person', 1: 'bicycle', 2: 'car', 3: 'motorcycle', 4: 'airplane', 5: 'bus', 6: 'train', 7: 'truck', 8: 'boat', 9: 'traffic light', 10: 'fire hydrant', 11: 'stop sign', 12: 'parking meter', 13: 'bench', 14: 'bird', 15: 'cat', 16: 'dog', 17: 'horse', 18: 'sheep', 19: 'cow', 20: 'elephant', 21: 'bear', 22: 'zebra', 23: 'giraffe', 24: 'backpack', 25: 'umbrella', 26: 'handbag', 27: 'tie', 28: 'suitcase', 29: 'frisbee', 30: 'skis', 31: 'snowboard', 32: 'sports ball', 33: 'kite', 34: 'baseball bat', 35: 'baseball glove', 36: 'skateboard', 37: 'surfboard', 38: 'tennis racket', 39: 'bottle', 40: 'wine glass', 41: 'cup', 42: 'fork', 43: 'knife', 44: 'spoon', 45: 'bowl', 46: 'banana', 47: 'apple', 48: 'sandwich', 49: 'orange', 50: 'broccoli', 51: 'carrot', 52: 'hot dog', 53: 'pizza', 54: 'donut', 55: 'cake', 56: 'chair', 57: 'couch', 58: 'potted plant', 59: 'bed', 60: 'dining table', 61: 'toilet', 62: 'tv', 63: 'laptop', 64: 'mouse', 65: 'remote', 66: 'keyboard', 67: 'cell phone', 68: 'microwave', 69: 'oven', 70: 'toaster', 71: 'sink', 72: 'refrigerator', 73: 'book', 74: 'clock', 75: 'vase', 76: 'scissors', 77: 'teddy bear', 78: 'hair drier', 79: 'toothbrush'}
+
+
+@app.route('/predict')
+def hello_world():
+    image = read_image_bgr('000000008021.jpg')
+
+    # preprocess image for network
+    image = preprocess_image(image)
+    image, scale = resize_image(image)
+    start = time.time()
+    boxes = None
+    scores = None
+    labels = None
+    with graph.as_default():
+        boxes, scores, labels = model.predict_on_batch(np.expand_dims(image, axis=0))
+    print("processing time: ", time.time() - start)
+    boxes /= scale
+    print(boxes, scores, labels)
+    return 'done'
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0', port=6000)
